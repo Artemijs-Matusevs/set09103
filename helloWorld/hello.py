e5a3111cd9d11dfbc9a401425bd27223c381d80b
Artemijs Matusev <matu6ev@gmail.com>
2023-10-12 21:54:52 +0100

    Setting up basic redirect

--- helloWorld/hello.py
+++ helloWorld/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,3 +14,13 @@ def hello():
 @app.route('/goodbye/')
 def goodbye():
     return "Goodbye!"
+
+@app.route("/private")
+def private():
+    # Test for user log-in,
+    # Redirect if failed,
+    return redirect(url_for('login'))
+
+@app.route('/login')
+def login():
+    return "LOGIN PAGE"

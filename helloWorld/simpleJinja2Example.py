ae4ade82960bbeff090aec50939f244dbf2338f2
Artemijs Matusev <matu6ev@gmail.com>
2023-10-13 22:14:11 +0100

    Added an example conditiona Jinja template to the simpleJinja2Example.py app

--- helloWorld/simpleJinja2Example.py
+++ helloWorld/simpleJinja2Example.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
+#Simple example of rendering a Jinja2 template
 @app.route('/hello/<name>')
 def hello(name=None):
     user = {'name': name}
     return render_template('hello.html', user=user)
+
+
+#Rendering a Jinja2 template containing a conditional clause
+@app.route('/hi/')
+@app.route('/hi/<name>')
+def hi(name=None):
+    return render_template('conditional.html', name=name)
